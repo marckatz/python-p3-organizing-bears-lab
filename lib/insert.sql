cc9cc9d1a9cc61411b411faff507616b9c548c22
Marc Katz <marc.f.katz@gmail.com>
2023-07-13 11:18:30 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+    VALUES 
+        (1, "Mr. Chocolate", 7, "M", "Brown", "Angy", 1),
+        (2, "Rowdy", 11, "F", "Black", "Territorial", 1),
+        (3, "Tabitha", 5, "F", "Tabby", "Evil fucker", 1),
+        (4, "Sergeant Brown", "F", 26, "Brown", "Wanted for violations of the Geneva Convention", 1),
+        (5, "Melissa", 30, "F", "Black", "Too friendly", 1),
+        (6, "Grinch", 1, "M", "Green", "Actually a frog", 0),
+        (7, "Wendy", 25, "F", "Brown", "Plays the violin", 1),
+        (8, NULL, 9223372036854775807, "F", "ERROR", "ERROR", 0);
